c61692a3009f56acc193a81f78afa015c7cccb38
JuanGuillermoR <ruizalvarez2819@gmail.com>
2024-05-21 16:09:46 -0500

    fix: some modifications were made to the contact file

--- Project-Barber/src/js/main.js
+++ Project-Barber/src/js/main.js
@@ -1,18 +1,17 @@
 // Import our custom CSS
 import "../scss/main.scss";
 import "../scss/abstracts/_variables.scss";
-
-// Import alerts
-import { eventListener } from "./alerts";
-eventListener()
-
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
-
+// Import alerts
+// import { eventListener } from "./alerts";
 // Import footer
 import {navbarComponent} from "./components/navbar";
-navbarComponent()
-
 // Import footer
 import { footerComponent } from "./components/footer";
+
+
+
+navbarComponent()
 footerComponent()
+// eventListener()
